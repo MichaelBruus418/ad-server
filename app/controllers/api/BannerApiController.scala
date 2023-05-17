c2df48e4d5e9402e5c1e8f33a26c21548e6db43d
Michael Bruus <mb@michaelbruus.dk>
2023-05-17 15:44:24 +0200

    Ready for connection test of bannerApi

--- app/controllers/api/BannerApiController.scala
+++ app/controllers/api/BannerApiController.scala
@@ -8,27 +8,44 @@ import utils.AuthenticateUtil
 import javax.inject._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class BannerApiController @Inject()(
+class BannerApiController @Inject() (
   val controllerComponents: ControllerComponents,
   val zoneDao: ZoneDao,
 ) extends BaseController {
 
+  /*
+  * Simple connection test.
+  * Returns 200 for authorized access or 400/401/500 for unsuccessful access.
+  *  */
   def ping: Action[AnyContent] = Action {
     implicit request: Request[AnyContent] =>
-      val headers = request.headers.toString()
-      val bodyJson = request.body.asJson
-      println("Headers:")
-      println(headers)
-      println("Body:")
-      println(bodyJson)
-
-      val result = AuthenticateUtil.apiBasicAuthentication(request.headers)
-
-
-      Ok(Json.toJson("status: {'ping success'}"))
-
+      val result = authenticate(request.headers)
+      if (result.header.status != 200) {
+        result
+      }
+      else {
+        Ok
+      }
   }
 
 
-
+  private def authenticate(headers: Headers): Result = {
+      try {
+        if (AuthenticateUtil.apiBasicAuthentication(headers)) {
+          Status(200)
+        } else {
+          Status(401)("Not authorized.").withHeaders(
+            "WWW-Authenticate" -> "Basic"
+          )
+        }
+      } catch {
+        case e: IllegalArgumentException =>
+          Status(400)(s"Authorization validation error: ${e.getMessage}")
+            .withHeaders("WWW-Authenticate" -> "Basic")
+        case e: Throwable =>
+          Status(500)("Unknown Server Errpr").withHeaders(
+            "WWW-Authenticate" -> "Basic"
+          )
+      }
+  }
 }
--- app/utils/AuthenticateUtil.scala
+++ app/utils/AuthenticateUtil.scala
@@ -4,11 +4,15 @@ import play.api.libs.Comet.initialByteString.decodeBase64
 import play.api.mvc.Headers
 
 import java.util.Base64
+import scala.util.matching.Regex
 
 object AuthenticateUtil {
 
+  /*
+   * Authenticatino using Basic Auth.
+   * Reqquires Authorization field in request header containg base64 encoded userName and password.
+   *  */
   def apiBasicAuthentication(headers: Headers): Boolean = {
-
     headers
       .get("Authorization")
       .fold(false)(auth => {
@@ -16,23 +20,54 @@ object AuthenticateUtil {
         if (
           authValues.length == 2 && authValues.head.equalsIgnoreCase("Basic")
         ) {
-          val decoded              = new String(
+          val decoded      = new String(
             Base64.getDecoder.decode(authValues(1).getBytes("utf-8"))
           )
-          val credentials          = decoded.split(":")
-          val (username, password) = (credentials.head, credentials(1))
-          println("username: " + username + " password: " + password)
-          false
+          // As we use semicolon to seperate userName:password, check that exactly one is present.
+          val numOfMatches = ("(:)".r).findAllMatchIn(decoded).length
+          if (numOfMatches != 1)
+            throw new IllegalArgumentException(
+              "Authorization string invalid."
+            )
+          // Split into uswerName and password
+          val credentials  = decoded.split(":")
+          if (credentials.length == 2) {
+            authenticateByNameAndPassword(credentials.head, credentials(1))
+          } else {
+            false
+          }
         } else {
           false
         }
       })
   }
 
-  def authenticateByNameAndPassword(
-    username: String,
+  private def authenticateByNameAndPassword(
+    userName: String,
     password: String,
   ): Boolean = {
-    false
+    // Hard-coded credentials for the time being.
+    // ..and no, passwords shouldn't be stored in plain-text,
+    // but this is only a proof-of-concept Ad-Server, so chill!
+    val credentials: Map[String, String] = Map(
+      "Jyllands-Posten" -> "1234",
+      "Finans"          -> "5678",
+    )
+
+    // Hack to ignore case in map keys
+    val index = credentials.keys
+      .map(_.toLowerCase())
+      .toIndexedSeq
+      .indexOf(userName.toLowerCase())
+    println("index: " + index)
+    if (index != -1)
+      println("retrieved password: " + credentials.values.toIndexedSeq(index))
+    if (
+      index != -1 && password.equals(credentials.values.toIndexedSeq(index))
+    ) true
+    else false
+
+    // Why doesnt this return an Option[String)?
+    // val password = credentials.getOrElse(userName, credentials.get(userName.toLowerCase()))
   }
 }
